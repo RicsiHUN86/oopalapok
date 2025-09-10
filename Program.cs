7b9475ffe824279db2f4a81a9d12048b4f911956
BRicsi <barnusrichi@gmail.com>
2025-09-10 08:31:10 +0200

    Feladat1 is ready

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace oopgyak
 {
+    public class szemely
+    {
+        public string nev = "Ádám";
+        public int kor = 34;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            szemely tanulo1 = new szemely();
+            Console.WriteLine($"A tanulo neve: {tanulo1.nev} és {tanulo1.kor} éves!");
         }
     }
 }
